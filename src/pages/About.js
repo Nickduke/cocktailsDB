1c06180509199c4deff6dfeb8c732df301994661
Nick <ntoukas.nick@gmail.com>
2023-02-04 15:08:04 -0500

    complete error and about pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita veniam
+        tempore minima commodi. Officia distinctio itaque, eveniet dolore natus
+        magni commodi? Maiores labore unde earum quisquam? Dolor nesciunt dicta
+        amet veritatis sit necessitatibus ipsa, saepe maiores fugit, quo est
+        tempora.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
